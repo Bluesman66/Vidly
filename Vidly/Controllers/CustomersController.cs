b94af22d1db7a027900de18735d98ffa670eb5f1
alex.paclin <alex.paclin@gmail.com>
2017-01-27 17:54:46 +0500

    043-Exercise

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -34,6 +34,22 @@ namespace Vidly.Controllers
 			return View("CustomerForm", viewModel);
 		}
 
+		public ActionResult Edit(int id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if (customer == null)
+				return HttpNotFound();
+
+			var viewModel = new CustomerFormViewModel
+			{
+				Customer = customer,
+				MembershipTypes = _context.MembershipTypes.ToList()
+			};
+
+			return View("CustomerForm", viewModel);
+		}
+
 		[HttpPost]
 		public ActionResult Save(Customer customer)
 		{
@@ -67,22 +83,6 @@ namespace Vidly.Controllers
 				return HttpNotFound();
 
 			return View(customer);
-		}
-
-		public ActionResult Edit(int id)
-		{
-			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
-
-			if (customer == null)
-				return HttpNotFound();
-
-			var viewModel = new CustomerFormViewModel
-			{
-				Customer = customer,
-				MembershipTypes = _context.MembershipTypes.ToList()
-			};
-
-			return View("CustomerForm", viewModel);
 		}
 	}
 }
--- Vidly/ViewModels/CustomerFormViewModel.cs
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -12,5 +12,16 @@ namespace Vidly.ViewModels
 
 		public Customer Customer { get; set; }
 
+		public string Title
+		{
+			get
+			{
+				if (Customer != null && Customer.Id != 0)
+					return "Edit Customer";
+
+				return "New Customer";
+			}
+		}
+
 	}
 }
