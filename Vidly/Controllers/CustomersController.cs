25dabad90ab425d7860adc2fc20e6d84b9a96225
alex.paclin <alex.paclin@gmail.com>
2017-02-08 15:51:16 +0500

    098-Data Cache

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -79,6 +80,12 @@ namespace Vidly.Controllers
 
 		public ActionResult Index()
 		{
+			//if (MemoryCache.Default["Genres"] == null)
+			//{
+			//	MemoryCache.Default["Genres"] = _context.Customers.ToList();
+			//}
+
+			//var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
 			return View();
 		}
 
